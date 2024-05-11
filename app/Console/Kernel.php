b29a5fdfae7d3ccd5a325ed18e4934f3a986b114
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-11 22:55:49 +0300

    first commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console;
+
+// use Http;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->everyFiveSeconds()->output();
+        $schedule->command('echo hello')->everyFiveSeconds();
+        $schedule->call(
+            function () {
+                $response = Http::get('http://localhost:8000');
+
+            }
+        )->appendOutputTo('scheduler-output.log');
+
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
