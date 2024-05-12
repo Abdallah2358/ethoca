d71502c12645a3e61d4d1cd22445f0d8827c5b5a
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-12 22:25:53 +0300

    chore: Add EthocaAlert model and migration files

---
+++ database/migrations/2024_05_12_145419_create_ethoca_alerts_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ethoca_alerts', function (Blueprint $table) {
+            $table->id();
+            $table->string('ethoca_id', 25)->comment('Ethoca generated unique ID for the alert');
+            $table->string('type',30)->comment('The alert type: sourced from issuer/cardscheme or dispute');
+            $table->dateTime('alert_timestamp')->comment('The date and time the alert was available to send in the member’s time zone');
+            $table->string('age', 50)->comment('Numeric age of the alert. It is the number of hours between the transaction date/time (authorisation date/time) and the AlertTimestamp');
+            $table->string('issuer', 100)->comment('The name of the card issuer');
+            $table->string('card_number', 19)->comment('Numeric card number. This will be provided unmasked / masked depending on the member’s Ethoca account settings.');
+            $table->string('card_bin', 6)->comment('The first 6 digits of the card number');
+            $table->string('card_last4', 4)->comment('The last 4 digits of the card number');
+            $table->string('arn', 64)->comment('Acquirer Reference Number (ARN) isa unique 23/24-digit number that tagsa card transaction when it goes fromthe merchant\'s bank (the acquiringbank) through the card scheme to thecardholder\'s bank (the issuer). It isonly available for settled transactions');
+            $table->dateTime('transaction_timestamp')->comment('This is the authorization date/time for the transaction. YYYY-MM-DDThh:mm:ss in the member’s time zone (e.g. in EST)');
+            $table->string('merchant_descriptor', 50)->comment('The merchant descriptor / name as seen on the card holder’s statement');
+            $table->integer('member_id')->comment('A unique ID for each Ethoca member. For partners receiving alerts on behalf of merchants then this will be the MemberID for the merchant');
+            $table->string('mcc', 4)->comment('Merchant Category Code - defines the industry to which the transaction belongs');
+            $table->decimal('amount', 10, 2)->comment('Transaction amount');
+            $table->string('currency', 3)->comment('Currency for the transaction amount');
+            $table->string('transaction_type')->comment('The type of transaction or POS type');
+            $table->string('initiated_by')->comment('Whether the transaction was initially identified by the card holder or the card issuer');
+            $table->string('is_3d_secure')->comment('Whether it is a 3D Secure transaction or not.');
+            $table->string('source', 50)->comment('This field indicates whether an alert was originated directly from an issuer, or from another source');
+            $table->string('auth_code', 8)->comment('Authorization code for this alert');
+            $table->string('merchant_member_name', 60)->comment('The merchant name of the Ethoca member.');
+            $table->string('transaction_id', 65)->comment('A unique reference number must be flowed throughout the lifecycle of all card transactions.');
+            $table->string('chargeback_reason_code', 30)->comment('The card scheme-specific chargeback reason code which indicates the cardholder’s reason for disputing the transaction.');
+            $table->decimal('chargeback_amount', 10, 2)->comment('Chargeback Amount');
+            $table->string('chargeback_currency', 3)->comment('Currency for the chargeback amount.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ethoca_alerts');
+    }
+};
