cad5cf596836f1f1b952981fc01e4c289be7739f
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-14 17:25:49 +0300

    added an ethoca errors table to track all ethoca related errors

--- app/Models/EthocaError.php
+++ app/Models/EthocaError.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EthocaError extends Model
 {
     use HasFactory;
+    public function error_origin(): BelongsTo
+    {
+        return $this->belongsTo($this->model, $this->model_id);
+    }
 }
--- database/migrations/2024_05_12_235400_create_ethoca_errors_table.php
+++ database/migrations/2024_05_12_235400_create_ethoca_errors_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,10 +13,12 @@ return new class extends Migration
         Schema::create('ethoca_errors', function (Blueprint $table) {
             $table->id();
             $table->string('model')->comment('The model that the error is associated with');
+            $table->bigInteger('model_id');
             $table->string('code', 10)->comment('The error code returned by Ethoca');
             $table->string('description', 255)->comment('The error message returned by Ethoca');
             $table->timestamps();
         });
+
     }
 
     /**
