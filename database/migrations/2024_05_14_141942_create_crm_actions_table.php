6d2ef72fcb2edec516c4168bc5247ad9efb61680
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-14 17:25:06 +0300

    added CRM Actions table to track actions done to handle an Alert

---
+++ database/migrations/2024_05_14_141942_create_crm_actions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\EthocaAlert;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('crm_actions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(EthocaAlert::class)->comment('Alert id that is caused this action to trigger');
+            $table->string('name')->comment("A Descriptive name for action take on CRM");
+            $table->string('link')->comment("Link used to show the action details on CRM");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('crm_actions');
+    }
+};
