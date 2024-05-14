d4e538e8d132e7a83338ca1112c76455f35980f9
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-14 17:39:09 +0300

    finished alerts pull command waiting testing

--- app/Console/Commands/MakeSoapAlertsRequest.php
+++ app/Console/Commands/MakeSoapAlertsRequest.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\EthocaAlert;
 use App\Models\EthocaAlertError;
 use App\Models\EthocaAlertResponse;
+use App\Models\EthocaError;
 use Illuminate\Console\Command;
 
 use Illuminate\Support\Facades\Http;
@@ -30,33 +31,51 @@ class MakeSoapAlertsRequest extends Command
      */
     public function handle()
     {
-        $phpFilePath = public_path("EthocaAlerts-Sandbox.wsdl");
+        // start connection
+        $phpFilePath = env('ETHOCA_WSDL_URL', public_path("EthocaAlerts-Sandbox.wsdl"));
         $client = new \SoapClient($phpFilePath);
-        $creds = array(["Username" => "Steveasdf", "Password" => "NetworkAdministrator"]);
+        $creds = array(["Username" => env('ETHOCA_USERNAME'), "Password" => env('ETHOCA_PASSWORD')]);
+
+        // call Ethoca360Alerts
         $alert_response = $client->__soapCall("Ethoca360Alerts", $creds, ["trace" => true, "_connection_timeout" => 180]);
+        // log response to console
         $this->info(json_encode($alert_response));
+
+        // store response in db
         $alert_res_model = EthocaAlertResponse::create([
             'major_code' => $alert_response->majorCode,
             'status' => $alert_response->Status,
             'number_of_alerts' => $alert_response->numberOfAlerts ?? 0,
         ]);
+
+        // if it is saved successfully
         if ($alert_res_model->id) {
+            // save all errors to database
             foreach ($alert_response->Errors as $error) {
-                EthocaAlertError::create([
-                    'ethoca_alert_response_id' => $alert_res_model->id,
+                EthocaError::create([
+                    'model' => EthocaAlertResponse::class,
+                    'model_id' => $alert_res_model->id,
                     'code' => $error->code,
                     'description' => $error->_,
                 ]);
             }
+
+            // if the call is successful store all alerts in database
             if ($alert_response->majorCode == 0) {
                 foreach ($alert_response->Alerts as $alert) {
-                    EthocaAlert::create($alert);
+                    EthocaAlert::create($alert); // assuming the alert response felids names match database names
+                    // TODO : Check this in testing to make sure they match
                 }
-                foreach (array_chunk($alert_response->Alerts,24,true) as $alertsArrays) {
+
+                // Now Start Acklogment of Received Alerts
+                // chunk the alerts to 24 sized arrays to follow ethoca docs
+                foreach (array_chunk($alert_response->Alerts, 24, true) as $alertsArrays) {
                     foreach ($alertsArrays as $alerts) {
                         $args = array(array_merge($creds, ["Alerts" => $alerts]));
                         $ack_response = $client->__soapCall("EthocaAlertAcknowledgement ", $args, ["trace" => true, "_connection_timeout" => 180]);
+                        // Log the ack response to console
                         $this->info(json_encode($ack_response));
+                        // TODO : Save Ack request and response if needed
                     }
                 }
             }
