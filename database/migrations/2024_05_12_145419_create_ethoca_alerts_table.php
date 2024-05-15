a5d54c24ac97520219fd19b5c5f97241d900c79c
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-15 03:31:40 +0300

    chore: Add CRM Transaction ID and flags to Ethoca Alerts table

--- database/migrations/2024_05_12_145419_create_ethoca_alerts_table.php
+++ database/migrations/2024_05_12_145419_create_ethoca_alerts_table.php
@@ -14,6 +14,9 @@ return new class extends Migration {
         Schema::create('ethoca_alerts', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(EthocaAlertResponse::class)->comment('Associated Response ID');
+            $table->mediumInteger('crm_transaction_id')->comment('CRM Transaction ID');
+            $table->boolean('is_handled')->comment('This flag is raised when the CRM is done handling this alert and waiting update');
+            $table->boolean('is_updated')->comment('This flag is raised when the alert is successfully state updated with ethoca'); # TODO : Check if it needs to be an enum instead
             $table->string('ethoca_id', 25)->comment('Ethoca generated unique ID for the alert');
             $table->string('type', 30)->comment('The alert type: sourced from issuer/cardscheme or dispute');
             $table->dateTime('alert_timestamp')->comment('The date and time the alert was available to send in the member’s time zone');
@@ -35,7 +38,7 @@ return new class extends Migration {
             $table->string('source', 50)->comment('This field indicates whether an alert was originated directly from an issuer, or from another source');
             $table->string('auth_code', 8)->comment('Authorization code for this alert');
             $table->string('merchant_member_name', 60)->comment('The merchant name of the Ethoca member.');
-            $table->string('transaction_id', 65)->comment('A unique reference number must be flowed throughout the lifecycle of all card transactions.');
+            $table->string('ethoca_transaction_id', 65)->comment('A unique reference number must be flowed throughout the lifecycle of all card transactions.');
             $table->string('chargeback_reason_code', 30)->comment('The card scheme-specific chargeback reason code which indicates the cardholder’s reason for disputing the transaction.');
             $table->decimal('chargeback_amount', 10, 2)->comment('Chargeback Amount');
             $table->string('chargeback_currency', 3)->comment('Currency for the chargeback amount.');
