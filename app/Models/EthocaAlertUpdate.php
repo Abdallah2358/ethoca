4a4220c1e3d892938eae2e114a48e7fe98e09314
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-15 15:36:31 +0300

    Refactor EthocaAlert model to improve getUpdateForRequest method

--- app/Models/EthocaAlertUpdate.php
+++ app/Models/EthocaAlertUpdate.php
@@ -10,4 +10,18 @@ class EthocaAlertUpdate extends Model
     use HasFactory;
     protected $table = 'ethoca_alert_updates';
     protected $guarded = [];
+    public function getUpdateForRequest()
+    {
+        $alert_update = array();
+        $optionalEthocaName = ['Refunded', 'FirstPartyFraud', 'AmountStopped', 'ActionTimestamp'];
+        $optional = ['refunded', 'first_party_fraud', 'amount_stopped', 'action_timestamp'];
+        $alert_update['EthocaID'] = $this->ethoca_id;
+        $alert_update['Outcome'] = $this->outcome;
+        for ($i = 0; $i < count($optional); $i++) {
+            if ($this->$optional[$i] != null) {
+                $alert_update[$optionalEthocaName[$i]] = $this->{$optional[$i]};
+            }
+        }
+        return $alert_update;
+    }
 }
