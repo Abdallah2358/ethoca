ab04b96e9ef4ce8d56fb82fd6f2281e1338531bd
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-15 17:22:52 +0300

    Refactor EthocaAlertUpdates migration to add nullable and default values and some indexs

--- database/migrations/2024_05_15_003704_create_ethoca_alert_updates_table.php
+++ database/migrations/2024_05_15_003704_create_ethoca_alert_updates_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\EthocaAlert;
+use App\Models\EthocaAlertResponse;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,10 +18,11 @@ return new class extends Migration {
             $table->foreignIdFor(EthocaAlert::class)->comment('The alert id');
             // redundant field as we can get the alert id from the alert model
             // but it is used to reduce the number of queries since time is more important than space
-            $table->string('ethoca_id', 25)->comment('The Ethoca ID');
-            $table->string('outcome', 50)->comment('The outcome of the alert');
-            $table->string('refunded', 50)->comment('The refunded amount'); // can be refactored to tiny int
-            $table->boolean('first_party_fraud')->comment('The first party fraud amount');
+            $table->string('ethoca_id', 25)->comment('The Ethoca ID')->nullable()->default(null)->index();
+
+            $table->string('outcome', 50)->comment('The outcome of the alert')->index();
+            $table->string('refunded', 50)->comment('The refunded amount')->index(); // can be refactored to tiny int
+            $table->boolean('first_party_fraud')->comment('The first party fraud amount')->index();
             $table->decimal('amount_stopped', 10, 2)->comment('The amount stopped');
             $table->dateTime('action_timestamp')->comment('The date and time the transaction was refunded / action taken as a result of the alert.');
             $table->string('update_comment', 250)->comment('Comment for the update - Additional comments to be provided with the
@@ -28,8 +30,7 @@ return new class extends Migration {
             // $table->boolean('status')->comment('The status of the update 1 - success, 0 - failed');
             // we don't need the status field as we can check the is_sent field and for errors we can check the errors table
             // if we get a status failed from ethoca just keep the update record and log the error in the errors table
-            $table->boolean('is_sent')->comment('This flag is raised when the update is sent to ethoca Successfully');
-
+            $table->boolean('is_sent')->comment('This flag is raised when the update is sent to ethoca and received a status success from ethoca')->default(0)->index();
             $table->timestamps();
         });
     }
