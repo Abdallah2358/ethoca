a474a215010888931d62a7bf3ee496c63e146929
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-15 17:21:28 +0300

    added some fields to errors table

--- database/migrations/2024_05_12_235400_create_ethoca_errors_table.php
+++ database/migrations/2024_05_12_235400_create_ethoca_errors_table.php
@@ -16,6 +16,10 @@ return new class extends Migration {
             $table->bigInteger('model_id')->comment('The id associated with the model related to this error');
             $table->string('code', 10)->comment('The error code returned by Ethoca');
             $table->string('description', 255)->comment('The error message returned by Ethoca');
+            $table->string('notes', 255)->comment('The some notes about the error')
+                ->nullable()->default(null);
+            $table->boolean('is_ack_error')->comment('The flag to indicate if the error is an acknowledgement error specific for alerts')
+                ->default(0)->index();
             $table->timestamps();
         });
 
