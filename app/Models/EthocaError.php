a93a9f13fcc52cc33c0b605b5af81a1c3b6907da
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-16 14:15:04 +0300

    added a function to generate errors from soap response

--- app/Models/EthocaError.php
+++ app/Models/EthocaError.php
@@ -14,4 +14,22 @@ class EthocaError extends Model
     {
         return $this->belongsTo($this->model, $this->model_id);
     }
+
+    public static function generateErrorsFromResponse($response, $model)
+    {
+        $errors = [];
+        if (isset($response->Errors) && isset($response->Errors->Error)) {
+            foreach ($response->Errors->Error as $error) {
+
+                $error_record = EthocaError::create([
+                    'model' => get_class($model),
+                    'model_id' => $model->id,
+                    'code' => $error->code,
+                    'description' => $error->_,
+                ]);
+                array_push($errors, $error_record);
+            }
+        }
+        return $errors;
+    }
 }
