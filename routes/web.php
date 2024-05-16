53cc43e6eef4672b15b17ab23e58ac69f31838ff
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-16 17:40:44 +0300

    users example table

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::resource('alerts', EAlertController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\UsersController;
+
+Route::get('/users', [UsersController::class, 'index'])->name('users.index');
