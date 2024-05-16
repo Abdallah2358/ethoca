1a6727bf43aefeb1cc7e19786e642dd2e808a9ce
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-16 21:12:37 +0300

    added ethoca_fun_code to request migration

--- database/migrations/2024_05_10_145427_create_ethoca_requests_table.php
+++ database/migrations/2024_05_10_145427_create_ethoca_requests_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('ethoca_requests', function (Blueprint $table) {
             $table->id();
             $table->string('title')->comment('The title give to the request');
+            $table->unsignedTinyInteger('ethoca_fun_code')->comment('1 - Alerts , 2 - Ack , 3 - Update')->index();
             $table->string('alert_type')->comment('The type of the alert')->nullable()->default(null);
             $table->date('search_start_date')->comment('The start date of the search')->nullable()->default(null);
             $table->date('search_end_date')->comment('The end date of the search')->nullable()->default(null);
