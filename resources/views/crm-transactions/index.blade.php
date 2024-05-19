849c5b388e223211f5cb56948df419ef8bfa40bd
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-20 01:45:41 +0300

    chore: Update CRM transaction index view with sticky table header and added columns

--- resources/views/crm-transactions/index.blade.php
+++ resources/views/crm-transactions/index.blade.php
@@ -1,1 +1,35 @@
-<h1>Hello from Transaction Index</h1>
+<style>
+    th {
+        position: sticky;
+        top: 50px;
+        background: white;
+    }
+</style>
+<table style="text-align: center" border="1">
+    <thead>
+        <th>id</th>
+        <th>transactionId</th>
+        <th>parentTxnId</th>
+        <th>merchant</th>
+        <th>merchantDescriptor</th>
+        <th>merchantId</th>
+        <th>alerts</th>
+    </thead>
+    <tbody>
+        @foreach ($crmTransactions as $crmTransaction)
+            <tr>
+                <td>
+                    <a href="{{ route('crm-transactions.show', $crmTransaction->id) }}">
+                        {{ $crmTransaction->id }}
+                    </a>
+                </td>
+                <td>{{ $crmTransaction->transactionId }}</td>
+                <td>{{ $crmTransaction->parentTxnId }}</td>
+                <td>{{ $crmTransaction->merchant }}</td>
+                <td>{{ $crmTransaction->merchantDescriptor }}</td>
+                <td>{{ $crmTransaction->merchantId }}</td>
+                <td>{{ $crmTransaction->ethocaAlerts->count() }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
