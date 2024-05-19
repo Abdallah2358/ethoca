7605cd53cb64caba16da58363d936c9082e89bb0
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-19 12:33:45 +0300

    Refactor CrmAction model to include HasError trait

--- app/Models/CrmAction.php
+++ app/Models/CrmAction.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasError;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CrmAction extends Model
 {
-    use HasFactory;
+    use HasFactory, HasError;
 
     protected $table = 'crm_actions';
 
     protected $guarded = [];
 
-    public function ethocaAlert() : BelongsTo
+    public function ethocaAlert(): BelongsTo
     {
         return $this->belongsTo(EthocaAlert::class);
     }
