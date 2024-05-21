24afa81b63a4e1c330d62ecd46a8ab7f067fc1f3
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-21 13:58:52 +0300

    Update print table view for datatables

--- app/DataTables/UsersDataTable.php
+++ app/DataTables/UsersDataTable.php
@@ -7,37 +7,75 @@ use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\SearchPane;
 use Yajra\DataTables\Services\DataTable;
 
 class UsersDataTable extends DataTable
 {
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
-        return (new EloquentDataTable($query))->setRowId('id');
+        return (new EloquentDataTable($query))->setRowId('id')->searchPane(
+            /*
+             * This is the column for which this SearchPane definition is for
+             */
+            'user_id',
+
+            /*
+             * Here we define the options for our SearchPane. This should be either a collection or an array with the
+             * form:
+             * [
+             *     [
+             *          'value' => 1,
+             *          'label' => 'display value',
+             *          'total' => 5, // optional
+             *          'count' => 3 // optional
+             *     ],
+             *     [
+             *          'value' => 2,
+             *          'label' => 'display value 2',
+             *          'total' => 6, // optional
+             *          'count' => 5 // optional
+             *     ],
+             * ]
+             */
+            fn() => User::query()->select('id as value', 'name as label')->get(),
+
+            /*
+             * This is the filter that gets executed when the user selects one or more values on the SearchPane. The
+             * values are always given in an array even if just one is selected
+             */
+            function (\Illuminate\Database\Eloquent\Builder $query, array $values) {
+                return $query->whereIn('id', $values);
+            }
+        );
     }
 
     public function query(User $model): QueryBuilder
     {
         return $model->newQuery();
     }
-
     public function html(): HtmlBuilder
     {
         return $this->builder()
+            // ->searchPanes(SearchPane::make())
             ->setTableId('users-table')
             ->columns($this->getColumns())
-            
+
             ->minifiedAjax()
             ->orderBy(1)
             ->selectStyleSingle()
-            ->buttons([
-                Button::make('add'),
-                Button::make('excel'),
-                Button::make('csv'),
-                Button::make('pdf'),
-                Button::make('print'),
-                Button::make('reset'),
-                Button::make('reload'),
+            ->searchPanes(SearchPane::make())
+            ->addColumnDef([
+                'targets' => '_all',
+                'searchPanes' => [
+                    'show' => true,
+                    'vieTotal' => false,
+                    'viewCount' => false,
+                ],
+            ])
+            ->parameters([
+                'dom' => 'PBfrtip',
+                'buttons' => ['export', 'print', 'reset', 'reload'],
             ]);
     }
 
