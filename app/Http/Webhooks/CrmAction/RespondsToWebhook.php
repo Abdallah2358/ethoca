ce4aea79bf42331369a69937fcc6e380569770ff
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-22 17:41:46 +0300

    refactor: Update RespondsToWebhook to include 'alert_id' in the JSON response

--- app/Http/Webhooks/CrmAction/RespondsToWebhook.php
+++ app/Http/Webhooks/CrmAction/RespondsToWebhook.php
@@ -11,6 +11,10 @@ class RespondsToWebhook implements SpatieRespondsToWebhook
     public function respondToValidWebhook(Request $request, WebhookConfig $config): Response
     {
         // Handle the incoming webhook
-        return response()->json(['ethoca_id' => $request->input('ethoca_id'), 'status' => 'received']);
+        return response()->json([
+            'ethoca_id' => $request->input('ethoca_id'),
+            'status' => 'received',
+            'alert_id' => $request->input('alert_id')
+        ]);
     }
 }
