0d2a32358774f18800a8561dd43d85b5e3f22081
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-22 15:33:16 +0300

    chore: Update CSRF token exception and webhooks route for EthocaAlertNotification with the .env endpoints

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'EthocaAlertNotification',
+        env('ETHOCA_WEBHOOK_ENDPOINT', 'EthocaAlertNotification'),
     ];
 }
--- app/Jobs/ProcessWebhookJob.php
+++ app/Jobs/ProcessWebhookJob.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Models\EthocaAlert;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob as SpatieProcessWebhookJob;
+use Spatie\WebhookClient\Models\WebhookCall;
 
 class ProcessWebhookJob extends SpatieProcessWebhookJob
 {
@@ -25,7 +27,12 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
      */
     public function handle(): void
     {
-
-        // response()->json($this->webhookCall->payload);
+        $payload = $this->webhookCall->payload;
+        $payload['alert_timestamp'] = date_format(date_create($payload['alert_timestamp']), 'Y-m-d\TH:i:s.v');
+        $payload['transaction_timestamp'] = date_format(date_create($payload['transaction_timestamp']), 'Y-m-d\TH:i:s.v');
+        $alert = EthocaAlert::create($payload);
+        $alert->webhookCall()->associate($this->webhookCall);
+        $alert->save();
+        // throw new \Exception("Error Processing Request", 1);
     }
 }
