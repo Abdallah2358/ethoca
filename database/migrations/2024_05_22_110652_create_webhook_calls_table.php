1a1e88c6b6f51ed7d78b3dcdb8c409fb453f518c
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-22 17:36:01 +0300

    refactor: Add default value for 'name' column in webhook_calls table

--- database/migrations/2024_05_22_110652_create_webhook_calls_table.php
+++ database/migrations/2024_05_22_110652_create_webhook_calls_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('webhook_calls', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->string('name');
+            $table->string('name')->index()->default('default');
             $table->string('url');
             $table->json('headers')->nullable();
             $table->json('payload')->nullable();
