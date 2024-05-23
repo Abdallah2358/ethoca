b66ec971de5127cdaf6fb26886b7e3d989b8e62b
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-23 14:03:13 +0300

    refactor: Add foreign key for Company model in merchants table migration

--- database/migrations/2024_05_22_174124_create_merchants_table.php
+++ database/migrations/2024_05_22_174124_create_merchants_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Company::class);
+            $table->string('descriptor');
+            $table->string('mcc');
             $table->timestamps();
         });
     }
