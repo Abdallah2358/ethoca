e550ac0c1a93a28fac64486d22ef4535d74821d1
عبدالله الحديدي <abdallah201@protonmail.com>
2024-05-26 17:44:46 +0300

    refactor: Update EthocaRequest generateRequest method
    This commit updates the `generateRequest` method in the `EthocaRequest` model. It introduces the `ethoca_fun_code` variable to store the code corresponding to the `ethoc_function` parameter. This change enhances the readability and maintainability of the code by explicitly representing the function code.

--- app/Models/EthocaRequest.php
+++ app/Models/EthocaRequest.php
@@ -66,7 +66,8 @@ class EthocaRequest extends Model
     {
         return RequestFactory::new();
     }
-    public static function generateRequest(\SoapClient $client, $ethoc_function, $ethoca_args, $start = null, $end = null): mixed
+
+    public function generateRequest(\SoapClient $client, $ethoc_function, $ethoca_args, $start = null, $end = null): mixed
     {
         $title = 'Ethoca 360 Alerts Request';
         $alert_type = null;
@@ -74,33 +75,98 @@ class EthocaRequest extends Model
             case 'Ethoca360Alerts':
                 $title = 'Ethoca 360 Alerts Request';
                 $alert_type = $ethoca_args['AlertType'] ?? 'all'; // default to 'all
+                $ethoca_fun_code = 1;
                 break;
             case 'EthocaAlertAcknowledgement':
                 $title = 'Acknowledge Ethoca Alerts Request';
+                $ethoca_fun_code = 2;
                 break;
             case 'Ethoca360AlertsUpdate':
                 $title = 'Update Ethoca Alerts Request';
+                $ethoca_fun_code = 3;
                 break;
         }
         $ethoca_request = EthocaRequest::create([
             'title' => $title,
             'alert_type' => $alert_type, // default to 'all
+            'ethoca_fun_code' => $ethoca_fun_code,
             'search_start_date' => $ethoca_args['SearchStartDate'] ?? null,
             'search_end_date' => $ethoca_args['SearchEndDate'] ?? null,
         ]);
-        $response = $client->__soapCall($ethoc_function, $ethoca_args, ["trace" => true, "_connection_timeout" => 180]);
-        // save all errors to database
-        if (isset($response->Errors) && isset($response->Errors->Error)) {
-            foreach ($response->Errors->Error as $error) {
+        try {
+            $response = $client->__soapCall($ethoc_function, $ethoca_args, ["trace" => true, "_connection_timeout" => 180]);
+            $alert_res_model = EthocaResponse::create([
+                'major_code' => $response->majorCode,
+                'status' => $response->Status,
+                'number_of_alerts' => $response->numberOfAlerts ?? null,
+                'ethoca_request_id' => $ethoca_request->id ?? null,
+            ]);
+            if (isset($response->Errors) && is_array($response->Errors->Error)) {
+                foreach ($response->Errors->Error as $error) {
+                    EthocaError::create([
+                        'model' => EthocaResponse::class,
+                        'model_id' => $alert_res_model->id,
+                        'code' => $error->code,
+                        'description' => $error->_,
+                    ]);
+                }
+            } else {
                 EthocaError::create([
-                    'model' => EthocaRequest::class,
-                    'model_id' => $ethoca_request->id,
-                    'code' => $error->code,
-                    'description' => $error->_,
+                    'model' => EthocaResponse::class,
+                    'model_id' => $alert_res_model->id,
+                    'code' => $response->Errors->Error->code,
+                    'description' => $response->Errors->Error->_,
                 ]);
             }
+            $response->model = $alert_res_model;
+        } catch (\Throwable $th) {
+            EthocaError::create([
+                'model' => EthocaRequest::class,
+                'model_id' => $ethoca_request->id,
+                'code' => 500,
+                'description' => 'Failed to connect to Ethoca 360 Alerts API',
+            ]);
         }
-        $response['ethoca_request_id'] = $ethoca_request->id;
+        // save all errors to database
+
+
         return $response;
     }
+    // public static function generateRequest(\SoapClient $client, $ethoc_function, $ethoca_args, $start = null, $end = null): mixed
+    // {
+    //     $title = 'Ethoca 360 Alerts Request';
+    //     $alert_type = null;
+    //     switch ($ethoc_function) {
+    //         case 'Ethoca360Alerts':
+    //             $title = 'Ethoca 360 Alerts Request';
+    //             $alert_type = $ethoca_args['AlertType'] ?? 'all'; // default to 'all
+    //             break;
+    //         case 'EthocaAlertAcknowledgement':
+    //             $title = 'Acknowledge Ethoca Alerts Request';
+    //             break;
+    //         case 'Ethoca360AlertsUpdate':
+    //             $title = 'Update Ethoca Alerts Request';
+    //             break;
+    //     }
+    //     $ethoca_request = EthocaRequest::create([
+    //         'title' => $title,
+    //         'alert_type' => $alert_type, // default to 'all
+    //         'search_start_date' => $ethoca_args['SearchStartDate'] ?? null,
+    //         'search_end_date' => $ethoca_args['SearchEndDate'] ?? null,
+    //     ]);
+    //     $response = $client->__soapCall($ethoc_function, $ethoca_args, ["trace" => true, "_connection_timeout" => 180]);
+    //     // save all errors to database
+    //     if (isset($response->Errors) && isset($response->Errors->Error)) {
+    //         foreach ($response->Errors->Error as $error) {
+    //             EthocaError::create([
+    //                 'model' => EthocaRequest::class,
+    //                 'model_id' => $ethoca_request->id,
+    //                 'code' => $error->code,
+    //                 'description' => $error->_,
+    //             ]);
+    //         }
+    //     }
+    //     $response['ethoca_request_id'] = $ethoca_request->id;
+    //     return $response;
+    // }
 }
