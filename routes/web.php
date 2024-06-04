b76798d49c48b1f998f1563ddc4b16daa6fe6140
Abdallah El Hadidi <51133202+Abdallah2358@users.noreply.github.com>
2024-06-04 10:03:59 +0300

    Update web.php to use the 'home' view instead of returning a JSON response

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response(['message' => 'hello']);
+    return view('home');
 });
 Route::resource('requests', ERequestController::class);
 Route::resource('crm-actions', CrmActionsController::class);
